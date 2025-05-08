805cbd89ef04bc39164e670494817f593e04bab9
Mani-sh27 <beingahuman2512gmail.com>
2025-05-08 10:29:22 +0530

    Auto-commit on file change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,1 @@
-// import React from 'react'
-
-// const App = () => {
-//   return (
-//     <div>
-//       <h1 className='text-red-600 font-'> hello developers</h1>
-//       </div>
-//   )
-// }
-
-// export default App
+// 
