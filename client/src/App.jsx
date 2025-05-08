5e675056e032ce36d01259906a6c2859ccaa45ed
Mani-sh27 <beingahuman2512gmail.com>
2025-05-08 10:34:17 +0530

    Auto-commit on file change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,11 @@
-const chokidar = require('chokidar');
-const simpleGit = require('simple-git');
-const git = simpleGit();
+import React from 'react'
 
-let timeout;
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-red-600 font-'> hello developers</h1>
+      </div>
+  )
+}
 
-const watcher = chokidar.watch('.', {
-  ignored: /(^|[\/\\])\../, // Ignore dotfiles
-  persistent: true,
-  ignoreInitial: true
-});
-
-watcher.on('all', (event, path) => {
-  console.log(`📦 Detected change: ${event} on ${path}`);
-  clearTimeout(timeout);
-
-  // Wait 10 seconds before committing and pushing
-  timeout = setTimeout(async () => {
-    try {
-      await git.add('.');
-      await git.commit('🚀 Daily auto-update', ['--amend', '--no-edit']); // overwrite previous
-      await git.push('origin', 'master', { '--force': null }); // force push updated commit
-      console.log('✅ Code updated in single commit!');
-    } catch (err) {
-      console.error('❌ Git error:', err.message);
-    }
-  }, 10000); // 10 seconds
-});
+export default App
